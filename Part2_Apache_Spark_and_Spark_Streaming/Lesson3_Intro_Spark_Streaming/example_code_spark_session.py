251f5421c574ebcaedda0a8ea3083558921923a0
nesreensada <nesreensada@gmail.com>
2020-05-15 01:57:03 +0300

    chore: adding lesson3 and part of lesson4

---
+++ Part2_Apache_Spark_and_Spark_Streaming/Lesson3_Intro_Spark_Streaming/example_code_spark_session.py
@@ -0,0 +1,42 @@
+# Please complete the TODO items below
+
+from pyspark.sql import SparkSession
+import pyspark.sql.functions as psf
+
+
+def explore_data():
+
+    # TODO build a spark session (SparkSession is already imported for you!)
+    spark = SparkSession.builder.master('local').appName('csv file loader').getOrCreate()
+
+    # couple ways of setting config
+    spark.conf.set("spark.executor.memory", '8g')
+    spark.conf.set('spark.executor.cores', '3')
+    spark.conf.set('spark.cores.max', '3')
+    spark.conf.set("spark.driver.memory", '8g')
+
+    # TODO set correct path for file_path using Pathlib
+    # TODO use a correct operator to load a csv file (cities.csv in your resources folder)
+
+    file_path = "resources/lesson1/csv/cities.csv"
+    df = spark.read.csv(file_path, header= True)
+
+    # view schema
+    df.printSchema()
+
+    # TODO create another dataframe, drop null columns for start_year
+    df_filtered = df.dropna(subset='start_year')
+
+    # TODO select start_year and country only and get distinct values
+    # TODO sort by start_year ascending
+
+    distinct_df = df_filtered.select("start_year", "country") \
+        .distinct().sort(psf.col("start_year").asc()).show()
+
+    # show distinct values
+    distinct_df.show()
+
+    # which country had the metro system the earliest?
+
+if __name__ == "__main__":
+    explore_data()
